a70cacbfd7e0263c53a2cb3d6192cce8194b13d6
Madhava Sai <73011030+madhavasai-15@users.noreply.github.com>
2021-01-08 22:10:46 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,42 @@
+class Food{
+    constructor(foodStock, lastFed){
+        this.image = loadImage("images/Milk.png");
+        this.foodStock = foodStock;
+        this.lastFed = lastFed;
+    }
+
+    getFoodStock(){
+        db.ref('/').on("value", (data)=>{
+            foodStock = data.val();
+        });
+    }
+
+    updateFoodStock(count){
+        db.ref('/').update({
+            Food: count
+        });
+    }
+
+    deductFood(){
+
+    }
+
+    display(){
+        var x = 80;
+        var y = 100;
+
+        imageMode(CENTER);
+        image(this.image, 720, 220, 70, 70);
+
+        if(this.foodStock!=0){
+            for(var i = 0; i < this.foodStock; i++){
+                if(i % 10 === 0){
+                    x = 80;
+                    y = y + 50;
+                }
+                image(this.image, x, y, 50, 50);
+                x = x + 30;
+            }
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,80 @@
+var dog, dogImage, happydogImage, foodS, foodStock;;
+var db;
+var feed, addFood;
+var fedTIme, lastFed;
+var foodObj;
+
+function preload(){
+  dogImage = loadImage("images/Dog.png");
+  happydogImage = loadImage("images/happydog.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+  db = firebase.database();
+
+  dog = createSprite(400, 300, 10, 10);
+  dog.addImage(dogImage);
+  dog.scale = 0.2;
+
+  foodStock = db.ref("Food");
+  foodStock.on("value", readStock);
+
+  foodObj = new Food(foodStock, lastFed);
+
+  feed = createButton("feed");
+  feed.position(700, 80);
+  feed.mousePressed(feedDog);
+  addFood = createButton("add food");
+  addFood.position(760, 80);
+  addFood.mousePressed(addFoods);
+
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  fedTIme = db.ref("FeedTime");
+  fedTIme.on("value", function(data){
+    lastFed = data.val();
+  });
+
+  foodObj.display();
+
+  drawSprites();
+  fill(255, 255, 254);
+  textSize(15);
+  if(lastFed >= 12){
+    text("Last Feed : " + lastFed%12 + "PM", 350, 30);
+  }else if(lastFed === 0){
+    text("Last Feed : 12 AM", 350, 30);
+  }else {
+    text("Last Feed :"+ lastFed +"AM", 350, 30);
+  }
+}
+
+function feedDog(){
+  dog.addImage(happydogImage);
+
+  //foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  
+  db.ref('/').update({
+    Food: foodObj.getFoodStock(),
+    FeedTime: hour()
+  });
+ 
+}
+
+function addFoods(){
+  foodS++;
+  db.ref('/').update({
+    Food: foodS
+  });
+}
+
+
+function readStock(data){
+  foodS = data.val();
+}
